03861dabea023d4c9da9da6649ed5026cbdb0a6b
Mario Souto <mariosoutoskn@gmail.com>
2017-04-05 23:26:13 -0300

    initial

---
+++ components/footer/index.js
@@ -0,0 +1,15 @@
+(function(){
+	
+	const name = "Mario";
+	console.log(`AAA ${name}`);
+	const obj = {
+		name: 'Maria',
+		age: 19,
+	}
+
+	funcTest = (...obj) => {
+		return obj;
+	};
+
+
+})();
---
+++ components/menu_global/index.php
@@ -0,0 +1,11 @@
+<?php  function comp_menu_global($type, $args) { ?>
+	<nav class="menu-global menu_global--<?php echo $type; ?>">
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</nav>
+<?php }  ?>
